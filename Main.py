dfffd92a51df0a3157a69b744ccdfcbf59dc6c5e
unknown <Cody_Rimes@yahoo.com>
2025-02-02 14:27:03 -0800

    Just putting some comments into my remote repository for now

---
+++ Main.py
@@ -0,0 +1,7 @@
+#Here is a playground where we will try importing a Python library known as "requests" to have
+#our local Pycharm IDE able to send out HTTP requests (the protocol most client/server relationships
+#on the internet use to talk to each other in the hypertext transfer protocol)
+#Those HTTP requests will be used to talk to an API, i.e. someone else's application out there on the internet
+#that could be used to provide us some data, or that we could modify/delete/create some data on their server/application
+#First we will need to use the "pip install requests" console terminal command in order to import our library into Pycharm
+#it to use the library
